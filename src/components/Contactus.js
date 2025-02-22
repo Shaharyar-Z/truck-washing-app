f4e7c01c1571b8160b7f1e79fa4c21a72fe1ff16
shaharyar.zaheer <registershaharyar@gmail.com>
2025-02-22 14:33:51 +0500

    contact us change

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -4,175 +4,34 @@ import ContactUs from "../ContactUs";
 const Contactus = () => {
   return (
     <div>
-      <ContactUs />
-      {/* <div id="About" class="section contact">
-        <img
-          src="https://cdn.prod.website-files.com/64668e05ceb4d8ba0fa6789d/64668e05ceb4d8ba0fa6794a_shape_02.svg"
-          loading="lazy"
-          alt=""
-          class="shape _02"
-        />
+
+      <div id="Contact" class="section cta">
         <div class="content">
-          <div class="w-layout-grid _2-column-grid">
-            <div class="text-box _800px">
-              <div class="text-subtitle green">request for submission</div>
-              <h2 class="heading h2">
-                Let us know your
-                <span class="text-accent"> needs!</span>
-              </h2>
-              <p class="paragraph">
-                You will be supported within 24 hours of your request at the
-                latest, then we will set up an appointment to evaluate your
-                work!
-              </p>
-              <div class="form-block w-form">
-                <form
-                  data-name="Email Form"
-                  name="email-form"
-                  id="email-form"
-                  method="post"
-                  aria-label="Email Form"
-                >
-                  <div class="field-block">
-                    <label for="Nom" class="field-label">
-                      Name
-                    </label>
-                    <input
-                      class="text-input w-input"
-                      maxlength="256"
-                      name="Nom"
-                      data-name="Nom"
-                      placeholder=""
-                      type="text"
-                      id="Nom"
-                      required=""
-                    />
-                  </div>
-                  <div class="field-block">
-                    <label for="Adresse" class="field-label">
-                      Address
-                    </label>
-                    <input
-                      class="text-input w-input"
-                      maxlength="256"
-                      name="Adresse"
-                      data-name="Adresse"
-                      placeholder=""
-                      type="text"
-                      id="Adresse"
-                      required=""
-                    />
-                  </div>
-                  <div class="field-block">
-                    <label for="T-l-phone" class="field-label">
-                      Phone
-                    </label>
-                    <input
-                      class="text-input w-input"
-                      maxlength="256"
-                      name="T-l-phone"
-                      data-name="Téléphone"
-                      placeholder=""
-                      type="tel"
-                      id="T-l-phone"
-                      required=""
-                    />
-                  </div>
-                  <div class="field-block">
-                    <label for="Email-4" class="field-label">
-                      E-mail
-                    </label>
-                    <input
-                      class="text-input w-input"
-                      maxlength="256"
-                      name="Email"
-                      data-name="Email"
-                      placeholder=""
-                      type="email"
-                      id="Email-4"
-                      required=""
-                    />
-                  </div>
-                  <div class="field-block">
-                    <label for="Category" class="field-label">
-                      Services
-                    </label>
-                    <select
-                      data-name="Category"
-                      name="Category"
-                      required=""
-                      id="Category"
-                      multiple=""
-                      class="form-dropdown w-select"
-                    >
-                      <option value="Abattage">Tree felling</option>
-                      <option value="Élagage">Pruning</option>
-                      <option value="Essouchage ">Stump removal</option>
-                      <option value="Déboisement ">Deforestation</option>
-                      <option value="taille de haie">Hedge trimming</option>
-                      <option value="déchiquetage ">Shredding</option>
-                      <option value="Another option">Emergency 24/7</option>
-                    </select>
-                  </div>
-                  <div class="field-block">
-                    <label for="Message" class="field-label">
-                      Description of the work
-                    </label>
-                    <textarea
-                      placeholder=""
-                      maxlength="5000"
-                      data-name="Message"
-                      name="Message"
-                      id="Message"
-                      required=""
-                      class="text-input form w-input"
-                    ></textarea>
-                  </div>
-                  <div class="spacer _24"></div>
-                  <input
-                    type="submit"
-                    data-wait="Un instant..."
-                    class="secundary-button w-button"
-                    value="Send"
-                  />
-                </form>
-                <div
-                  class="form-success w-form-done"
-                  tabindex="-1"
-                  role="region"
-                  aria-label="Email Form success"
-                >
-                  <div class="success-text">
-                    Your message has been sent. We will get back to you as soon
-                    as possible.
-                  </div>
-                </div>
-                <div
-                  class="form-error w-form-fail"
-                  tabindex="-1"
-                  role="region"
-                  aria-label="Email Form Failure"
-                >
-                  <div class="error-text">
-                    An error occurred while sending your message. Please try
-                    again.
-                  </div>
-                </div>
+          <div class="cta-block">
+            <img
+              src="https://cdn.prod.website-files.com/64668e05ceb4d8ba0fa6789d/64668e05ceb4d8ba0fa67956_shape_06.svg"
+              loading="lazy"
+              alt=""
+              class="shape _05"
+            />
+            <img
+              src="https://cdn.prod.website-files.com/64668e05ceb4d8ba0fa6789d/64668e05ceb4d8ba0fa6794b_shape_05.svg"
+              loading="lazy"
+              alt=""
+              class="shape _06"
+            />
+            <div class="cta-text">
+              <div class="text-subtitle green centered">
+                don't wait any longer
               </div>
-            </div>
-            <div class="block-image">
-              <img
-                src="https://cdn.prod.website-files.com/64668e05ceb4d8ba0fa6789d/646905e96dbd64a648de17b1_AdobeStock_297381850-min.jpeg"
-                loading="lazy"
-                sizes="(max-width: 479px) 87vw, (max-width: 767px) 92vw, (max-width: 991px) 94vw, 92vw"
-                alt="A picture of two members of a team."
-                class="image"
-              />
-              <div class="image-animation-trigger"></div>
+              <h2 class="heading h2 cta">Contact us now!</h2>
+              <a href="/contact" class="primary-button cta w-button">
+                Get a quote
+              </a>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
